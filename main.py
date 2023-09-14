ca9a46075ad1ea76cf9281511f38cc8725503fd1
AleksanderWWW <alwojnarowicz@gmail.com>
2023-09-14 18:08:14 +0200

    minimalistic synchronous benchmark

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
+import time
 
-import httpx
 from dotenv import load_dotenv
 
 from src.neptune_benchmark.generate_runs import generate_runs
 from src.neptune_benchmark.generate_run_data import generate_run_data, generate_run_data_subset
 from src.neptune_benchmark.auth import get_auth_tokens
 from src.neptune_benchmark.request import fetch_chart_data
+from src.neptune_benchmark.constants import SUBSET_LENGTH
 
 
 def main():
+    execution_times = []
     load_dotenv()
     token = os.getenv("NEPTUNE_BENCHMARK_API_TOKEN")
     project = os.getenv("NEPTUNE_BENCHMARK_PROJECT")
@@ -21,11 +23,17 @@ def main():
     # refresh_token = auth_tokens.refreshToken
 
     all_run_data = generate_run_data(project, token)
-    subset = generate_run_data_subset(all_run_data, length=5)
 
-    response = fetch_chart_data(subset, project, access_token)
+    def single_task():
+        subset = generate_run_data_subset(all_run_data, length=SUBSET_LENGTH)
+        now = time.time()
+        fetch_chart_data(subset, project, access_token)
+        execution_times.append(time.time() - now)
 
-    print(len(response.json()["elements"]))
+    for i in range(10):
+        single_task()
+
+    print(execution_times)
 
 
 if __name__ == "__main__":
---
+++ src/neptune_benchmark/constants.py
@@ -0,0 +1,1 @@
+SUBSET_LENGTH = 5
