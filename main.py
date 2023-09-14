9cd98f63d06ccaaa9bf23f116004fbe58d659f3b
AleksanderWWW <alwojnarowicz@gmail.com>
2023-09-14 18:31:59 +0200

    make it async (needs optimizing)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import asyncio
 import os
 import time
 
+import httpx
 from dotenv import load_dotenv
 
 from src.neptune_benchmark.generate_runs import generate_runs
@@ -10,7 +12,7 @@ from src.neptune_benchmark.request import fetch_chart_data
 from src.neptune_benchmark.constants import SUBSET_LENGTH
 
 
-def main():
+async def main():
     execution_times = []
     load_dotenv()
     token = os.getenv("NEPTUNE_BENCHMARK_API_TOKEN")
@@ -24,17 +26,38 @@ def main():
 
     all_run_data = generate_run_data(project, token)
 
-    def single_task():
+    client = httpx.AsyncClient(
+        params={
+            'projectIdentifier': project,
+        },
+        headers={
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0',
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'authorization': f'Bearer {access_token}',
+            'content-type': 'application/json',
+            'Origin': 'https://testing.stage.neptune.ai',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+        })
+
+    async def single_task():
         subset = generate_run_data_subset(all_run_data, length=SUBSET_LENGTH)
         now = time.time()
-        fetch_chart_data(subset, project, access_token)
+        await fetch_chart_data(subset, client)
         execution_times.append(time.time() - now)
 
-    for i in range(10):
-        single_task()
+    tasks = [
+        single_task() for _ in range(10)
+    ]
 
+    await asyncio.gather(*tasks)
+
+    await client.aclose()
     print(execution_times)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
